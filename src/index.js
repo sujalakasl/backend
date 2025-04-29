965fcd66ef112afa638534875e8232b7bb203d06
Sujalakasl <sujallamichhaneakasl@gmail.com>
2025-04-30 00:20:19 +0545

    implement CRON to prevent API server sleep

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import "dotenv/config";
+import job from "./lib/cron.js";
 
-import authRoutes from './routes/authRoutes.js';
-import bookRoutes from './routes/bookRoutes.js';
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
 
-import { connectDB } from './lib/db.js';
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start(); // Start the cron job
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cors()); // Enable CORS for all routes
 
@@ -17,6 +19,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
---
+++ src/lib/cron.js
@@ -0,0 +1,19 @@
+import cron from "cron";
+import http from "https";
+
+const job = new cron.CronJob("*/14 * * * *", function () {
+  https
+    .get(process.env.API_URL, (res) => {
+      if (res.statusCode === 200) {
+        console.log("GET request sent successfully");
+      } else console.log("GET request failed", res.statusCode);
+    })
+    .on("error", (e) => {
+      console.error("Error with GET request:", e);
+    });
+});
+
+export default job;
+
+//CRON job explanation
+// The cron job is set to run every 14 minutes (*/14 * * * *).
