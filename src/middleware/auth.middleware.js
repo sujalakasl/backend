0161ae668c2e91249a673adda85646966cf51f06
Sujalakasl <sujallamichhaneakasl@gmail.com>
2025-05-15 11:14:03 +0545

    Revert middleware changes.

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -3,25 +3,26 @@ import User from "../models/User.js";
 
 const protectRoute = async (req, res, next) => {
   try {
-    const authHeader = req.headers["authorization"];
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    const token = req.headers("Authorization").replace("Bearer ", "");
+
+    if (!token)
       return res
         .status(401)
         .json({ message: "No token provided. Access denied" });
-    }
 
-    const token = authHeader.replace("Bearer ", "");
+    //verify token
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+    //find user by id
+
     const user = await User.findById(decoded.id).select("-password");
-    if (!user) {
+    if (!user)
       return res.status(401).json({ message: "Invalid token. Access denied" });
-    }
 
-    req.user = user;
+    req.user = user; // Attach user to request object
 
-    next();
+    next(); // Call the next middleware or route handler if everything is successful
   } catch (error) {
     console.log("Error in auth middleware", error);
     res.status(500).json({ message: error.message });
